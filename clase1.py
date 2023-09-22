52c61e7b8e04a162fc95f0d27fcf080ae897417f
AgustinMedina21 <agus.demian21@gmail.com>
2023-09-22 17:44:39 -0300

    Primeros pasos en Python

---
+++ clase1.py
@@ -0,0 +1,10 @@
+# ingreso = input("ingrese algo:") #leer ingreso
+# print("Termino el codigo, ingresaste",ingreso)
+
+# num1 = input("ingrese el primer numero: ")
+# num2 = input("ingrese el segundo numero: ")
+# print("El resultado de la suma es:", int(num1) + int(num2))
+
+num1 = int(input("ingrese el primer numero: "))
+num2 = int(input("ingrese el segundo numero: "))
+print("El resultado de la suma es:", num1 + num2)
