b40bd37735221d66daaca12140c60669d4d6bd12
AgustinMedina21 <agus.demian21@gmail.com>
2023-09-26 17:14:13 -0300

    Clase 2

---
+++ clase2.py
@@ -0,0 +1,19 @@
+# numero = 5
+# print("El numero es:", numero+2)
+
+# nombre = input()
+# print("Tu nombre es:", nombre)
+
+# edad = int(input("cuantos años tienes? "))
+# print("tienes", edad, "años y en 2 años vas a tener",# edad+2,"años")
+
+# nombre = "Jose"
+# edad = 25
+# print("tu nombre es",nombre,"y tienes",edad,"años")
+# print("tu nombre es {} y tienes {} años".format(nombre,edad))
+# print(f"tu nombre es {nombre} y tienes {edad} años")
+
+num1 = 10
+num2 = 3
+resultado = num1/num2
+print(f"el resultado de la suma es: {resultado:.2f}")
