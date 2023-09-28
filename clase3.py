fd2d6e9ef092edeb94340e627e3adc4cd8ed16fd
AgustinMedina21 <agus.demian21@gmail.com>
2023-09-28 15:34:01 -0300

    Clase 3

---
+++ clase3.py
@@ -0,0 +1,13 @@
+# cadena = "Hola mundo"
+# print("mundo" in cadena)
+
+# cadena = "Hola mundo"
+# print("mundo" not in cadena)
+
+#ACTIVIDAD
+
+# medida_en_mm = float(input("ingrese la medida en milimetros: "))
+# medida_en_pulgadas = medida_en_mm/25.4
+# print(f"{medida_en_mm} milimetros equivalen a {medida_en_pulgadas:.2f} pulgadas")
+
+print(f"Menu de opciones: \n1-Ventas \n2-Compras \n3-Reportes")  #Saltos de linea 
