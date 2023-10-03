985c1afddfd0e910e8993cc47efd8f624b54e50b
AgustinMedina21 <agus.demian21@gmail.com>
2023-10-03 15:41:06 -0300

    Clase 4

---
+++ clase4.py
@@ -0,0 +1,24 @@
+#num = int(input("ingrese un numero: "))
+#if num == 5:
+    # print("es un 5")
+# print("hola")
+
+# if 1 == 1:
+    #print("uno es uno")
+    #if 2 == 1:
+        #print("dos es uno")
+    #print("dos es uno")
+#print("chau")
+
+# edad = int(input("cuantos años tienes?"))
+# if edad < 18:
+    # print("eres menor de edad")
+# else:
+    # print("eres mayor de edad")
+
+nombre1 = input("ingrese el primer nombre:")
+nombre2 = input("ingrese el segundo nombre:")
+if nombre1 > nombre2:
+    print(f"{nombre1} es mayor que {nombre2}")
+else:
+    print(f"{nombre2} es mayor que {nombre1}")
