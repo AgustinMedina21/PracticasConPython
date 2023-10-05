2dd35d4f3a0a52225dbe3c1debece45661b6a049
AgustinMedina21 <agus.demian21@gmail.com>
2023-10-05 15:23:54 -0300

    Clase 5

---
+++ clase5.py
@@ -0,0 +1,20 @@
+# var = 0
+# while var < 5:
+    #print(var)
+    #var += 1 #var = var + 1
+#print("Me fui del var")
+
+cadena = "Soy una CaDEna de texto de PRUEBAS"
+
+#Sirve para contar la cantidad de caracteres
+#print(f"Hay {cadena.count('a')} cantidad de a en mi cadena")
+
+#Sirve para sacar las mayus de un texto
+#print(cadena.capitalize())
+#print(cadena)
+
+#Cada primera letra esta en mayuscula
+#print(cadena.title())
+
+print(cadena.upper())
+print(cadena.lower())
