c81d2d134d8089a1d9481c890e5a689146a8a4f2
AgustinMedina21 <agus.demian21@gmail.com>
2023-10-23 21:49:11 -0300

    Practicas

---
+++ print.py
@@ -0,0 +1,17 @@
+print("Hola mundo")
+print() #Salto de linea
+print("Hola mundo")
+
+
+print("Hola como estas?\nComo te llamas?")
+
+
+print("Hola chicos quiero contarles que me ire a pasear",end="                            "+"y la pasare bien\n") #comando end
+
+print("Lewandoski", "Ansu fati", "Ferran Torres", "Gavi", "Ter Stegen\n", sep="-") #comando sep
+
+#TAREA: REALIZAR UN PROGRAMA QUE MUESTRE POR CONSOLA LOS JUGADORES DEL REAL MADRID Y QUE SE SEPAREN POR COMAS Y LUEGO DEJAR UNA LINEA
+#Y COLOCAR LOS JUGADORES DEL ATLETICO MADRID
+
+print("Curtouis", "Alaba", "Carvajal", "rudiguer", "Modric\n", sep=",", end="-")
+print("Morata", "Joao felix", "Oblak", "Koke\n", sep=",")
---
+++ variables.py
@@ -0,0 +1,39 @@
+#INT = ENTERO
+#FLOAT/DOUBLE = DECIMAL
+#STRING
+#BOOLEAN
+
+num1 = 4
+num2 = 2
+
+print(num1)
+print(num2)
+
+print(num1 + num2)
+
+num1 = 4.7
+num2 = 2.5
+
+print(num1+num2)
+
+#INT = ENTERO
+#FLOAT = NUMEROS DECIMALES  
+
+num1 = "7"
+num2 = "2"
+
+print(num1+num2)
+
+num1 = "7"
+num2 = 2
+
+print(num1,num2)
+
+#STRING
+
+bol = True
+print(bol)
+
+bol1 = False
+print(bol1)
+
