d16d55c34148fb59d041df9063ff69b9dd403c66
AgustinMedina21 <agus.demian21@gmail.com>
2023-10-25 12:37:06 -0300

    Orden

---
+++ La casa infomatica/operadores.py
@@ -0,0 +1,43 @@
+#+
+#-
+#x
+#%
+#2 al cuadrado, 3 al cubo, 2 a la cuarta
+#raiz cuadrada de 4
+#residuo
+#division exacta
+
+num1 = 3
+num2 = 6
+suma1 = num1 + num2
+resta1 = num1 - num2
+suma2 = 4 + 5
+resta2 = 3-6
+print("La suma de los dos numeros es:", suma1, "\nla suma 2 es:", suma2)
+print("La resta de los dos numeros es:", resta1, "\nla resta 2 es:", resta2,"\n")
+
+#
+
+mul1 = int(input("ingrese un numero: "))
+mul2 = int(input("ingrese otro numero: "))
+
+print("La multiplicacion de los dos numeros es:", mul1 * mul2,"\n")
+
+#
+
+div = num1 / num2
+
+print("La division de los dos numeros es:", div, "\n")
+
+#
+
+potencia = num1 ** num2
+
+print("La potencia de los dos numeros es:", potencia, "\n")
+
+#
+
+raiz = num1 ** 0.5
+
+print("La raiz de los dos numeros es:", raiz, "\n")
+
